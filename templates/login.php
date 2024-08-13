109dd67f0652b954bbde29f6ccf0289a2ff3e5b4
ranjan gautam <ryanzan18@gmail.com>
2024-08-13 09:13:39 +0545

    feat(): bootstrap setup and login page design

--- templates/login.php
+++ templates/login.php
@@ -1,6 +1,21 @@
-<html>
-
+<html lang="en">
+<head>
+    <title>Login</title>
+    <?php include "layout/header.php" ?>;
+</head>
 <body>
-   <?= $authUrl ?>
+<div class="container">
+    <div class="row">
+        <div class="col d-flex
+                        align-items-center
+                        justify-content-center">
+            <a class="btn btn-outline-primary" href="<?= $authUrl ?>">
+                Sign up with Google
+            </a>
+        </div>
+    </div>
+</div>
+
+<?php include "layout/footer.php" ?>
 </body>
 </html>
